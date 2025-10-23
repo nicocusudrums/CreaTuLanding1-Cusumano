b36505ee04d9626792c6aa605c974945b602d3be
Nico Cusumano Cerasuolo <nicocusumanocerasuolo@gmail.com>
2025-10-23 11:38:52 -0300

    detalles

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/CreaTuLanding1-Cusumano/', 
+  base: "/CreaTuLanding1-Cusumano/",
   plugins: [react()],
 });
